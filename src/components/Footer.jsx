17ae871f75863f2dafd3fbf129b6e944ccbdc144
Kwadwo Sarbeng-Baafi <35800995+Sarbeng@users.noreply.github.com>
2022-10-24 22:55:49 +0000

    Footer file edited, index.css edited to style footer
    Edited the index.css file to make the footer more like the design, wrote my own css as tailwind wasnt doing what i wanted and was wasting too much time

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import zuri from '../assets/zuri_logo.svg'
+import zuri from '../assets/zuri.svg'
 import I4G from '../assets/I4G.svg'
+import '../index.css'
 
-export default function Footer(){
-    return(
-        <footer>
-            <div className="flex">
-                <img src={zuri} alt="" />
-                <p>HNG Internship 9 Frontend Task</p>
-                <img src={I4G} alt="" />
-             </div>
+export default function Footer() {
+    return (
+        <footer className="footer">
+
+            <div className="footer-items">
+                <img src={zuri} alt="" className="footer-left" />
+                <p className="footer-mid" >HNG Internship 9 Frontend Task</p>
+                <img src={I4G} alt="" className="footer-right" />
+
+            </div>
         </footer>
     )
 }
