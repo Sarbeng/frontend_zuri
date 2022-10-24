6022cb2c4c3217d2da00ff4b3842d22f7ca03f29
Kwadwo Sarbeng-Baafi <35800995+Sarbeng@users.noreply.github.com>
2022-10-24 15:30:35 +0000

    Header and Footer components Added
    I added the link to the output file in the dist folder as well as added two components, Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Links from './components/Links'
 import Profile from  './components/Profile'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Header />
       <Links />
       <Profile />
+      <Footer />
     </div>
   )
 }
