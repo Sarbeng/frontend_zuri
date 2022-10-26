4c87a32d56bffe1ed91c73cf1f6fdf6c54907373
Kwadwo Sarbeng-Baafi <35800995+Sarbeng@users.noreply.github.com>
2022-10-26 11:00:50 +0000

    style the share icon in the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import share from '../assets/share_button.svg'
 
 export default function Header(){
     return(
-        <div>
-         Header goes here
+        <div className="share">
+         <img src= {share} alt="" />
         </div>
     )
 }
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -3,7 +3,7 @@ import slack from '../assets/slack.svg'
 
 export default function Links(props) {
     return (
-        <div>
+        <div className="btn">
 
            <button className="btn-links">
            <a href={props.item.url} className={` links ${props.item.css_id}`}>{props.item.title}</a>
